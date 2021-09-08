3dcad6e64bb865e14fc78862d97b712ab9ba3b9d
RyanSyme <ryu321@hotmail.com>
2021-09-08 10:13:19 +0000

    add ghost movement functionality

--- index.js
+++ index.js
@@ -137,12 +137,41 @@ class Ghost {
     this.className = className
     this.startIndex = startIndex
     this.speed = speed
+    this.currentIndex = startIndex
+    this.isScrared = false
+    this.timerId = NaN
   }
 }
 
-ghosts = [
+const ghosts = [
   new Ghost('blinky', 348, 250),
   new Ghost('pinky', 376, 300),
   new Ghost('inky', 351, 350),
   new Ghost('clyde', 379, 400)
 ]
+
+ghosts.forEach(ghost => {
+  squares[ghost.startIndex].classList.add(ghost.className)
+  squares[ghost.startIndex].classList.add('ghost')
+})
+
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+  const directions = [-1, +1, -width, +width]
+  let direction = directions[Math.floor(Math.random() * directions.length)]
+  console.log(direction)
+
+  ghost.timerId = setInterval(function() {
+    if (
+      !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+      !squares[ghost.currentIndex + direction].classList.contains('ghost')
+    ) {
+      squares[ghost.currentIndex].classList.remove(ghost.className)
+      squares[ghost.currentIndex].classList.remove('ghost')
+      ghost.currentIndex += direction
+      squares[ghost.currentIndex].classList.add(ghost.className)
+      squares[ghost.currentIndex].classList.add('ghost')
+    } else direction = directions[Math.floor(Math.random() * directions.length)]
+  }, ghost.speed)
+}
